093c128318b7bd048563e5c4788469443e694f7d
hungnqph19112 <nqh1703k2@gmail.com>
2025-03-11 16:32:53 +0700

    refactoring IoC Container

---
+++ src/FStore.API/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿namespace FStore.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Add services to the container.
+
+            services.AddControllers();
+
+            services.AddEndpointsApiExplorer();
+
+            services.AddSwaggerGen();
+
+
+            return services;
+        }
+
+        public static WebApplication UseApiServices(this WebApplication app)
+        {
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            return app;
+        }
+    }
+}
--- src/FStore.API/Program.cs
+++ src/FStore.API/Program.cs
@@ -1,25 +1,28 @@
+using FStore.API;
+using FStore.Infrastructure;
+using FStore.Infrastructure.Persistence.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                     .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Add services to the container.
+builder.Services
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices(builder.Configuration);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseApiServices();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.ApplyMigrations();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
