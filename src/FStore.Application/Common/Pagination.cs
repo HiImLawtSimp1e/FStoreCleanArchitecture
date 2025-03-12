e66a38a34dea46f54108f9824c3635cb20d7b04b
hungnqph19112 <nqh1703k2@gmail.com>
2025-03-13 01:37:11 +0700

    Application: Common Model

---
+++ src/FStore.Application/Common/Pagination.cs
@@ -0,0 +1,9 @@
+﻿namespace FStore.Application.Common
+{
+    public class Pagination<T>
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public T? Result { get; set; }
+    }
+}
