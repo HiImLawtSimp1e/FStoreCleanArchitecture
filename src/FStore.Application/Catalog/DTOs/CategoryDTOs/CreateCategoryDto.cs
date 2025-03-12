f89fb8e5f6d007d9ce571a853f062b0f82d1f403
hungnqph19112 <nqh1703k2@gmail.com>
2025-03-13 01:34:31 +0700

    Application: add/update some Repo DTOs

--- src/FStore.Application/Catalog/DTOs/CategoryDTOs/CreateCategoryDto.cs
+++ src/FStore.Application/Catalog/DTOs/CategoryDTOs/CreateCategoryDto.cs
@@ -6,6 +6,10 @@
         [MinLength(2, ErrorMessage = "Category title must be at least 2 characters long")]
         [StringLength(50, ErrorMessage = "Category title must not exceed 50 characters")]
         public string Title { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Category slug is required")]
+        [MinLength(2, ErrorMessage = "Category slug must be at least 2 characters long")]
+        [StringLength(50, ErrorMessage = "Category slug must not exceed 50 characters")]
         public string Slug { get; set; } = string.Empty;
     }
 }
---
+++ src/FStore.Application/Catalog/DTOs/CategoryDTOs/CustomerCategoryResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FStore.Application.Catalog.DTOs.CategoryDTOs
+{
+    public class CustomerCategoryResponseDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Slug { get; set; } = string.Empty;
+    }
+}
